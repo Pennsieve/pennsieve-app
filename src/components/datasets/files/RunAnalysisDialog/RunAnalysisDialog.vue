7f1adc1ef2049d9d7723f7d44900b49c287ea070
Annalee Switek <annaleeswitek@ip-192-168-1-27.ec2.internal>
2024-05-29 16:11:58 -0400

    fixes

--- src/components/datasets/files/RunAnalysisDialog/RunAnalysisDialog.vue
+++ src/components/datasets/files/RunAnalysisDialog/RunAnalysisDialog.vue
@@ -396,7 +396,7 @@ export default {
         .then((response) => {
           EventBus.$emit("toast", {
             detail: {
-              msg: "The selected event has been successfully initiated!",
+              msg: "Your workflow has been successfully initiated!",
               type: "success",
             },
           });
@@ -569,5 +569,49 @@ export default {
 
 .margin {
   margin: 20px;
+}
+
+.lds-ring {
+  /* change color here */
+  color: #1c4c5b;
+}
+.lds-ring,
+.lds-ring div {
+  box-sizing: border-box;
+}
+.lds-ring {
+  display: inline-block;
+  position: relative;
+  width: 80px;
+  height: 80px;
+}
+.lds-ring div {
+  box-sizing: border-box;
+  display: block;
+  position: absolute;
+  width: 64px;
+  height: 64px;
+  margin: 8px;
+  border: 8px solid currentColor;
+  border-radius: 50%;
+  animation: lds-ring 1.2s cubic-bezier(0.5, 0, 0.5, 1) infinite;
+  border-color: currentColor transparent transparent transparent;
+}
+.lds-ring div:nth-child(1) {
+  animation-delay: -0.45s;
+}
+.lds-ring div:nth-child(2) {
+  animation-delay: -0.3s;
+}
+.lds-ring div:nth-child(3) {
+  animation-delay: -0.15s;
+}
+@keyframes lds-ring {
+  0% {
+    transform: rotate(0deg);
+  }
+  100% {
+    transform: rotate(360deg);
+  }
 }
 </style>
