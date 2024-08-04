377c98a14cb845a01f576c103f1b0f832d0a1eda
vijay kumar, samayamantula <vijay@sparc.science>
2024-08-04 15:13:39 -0400

    add scaffolding for releases page

---
+++ src/components/releases/github-repositories/GitHubRepositories.vue
@@ -0,0 +1,68 @@
+<template>
+  <bf-stage element-loading-background="transparent">
+
+    <bf-empty-page-state class="empty">
+      <p>There are no github repositories yet</p>
+    </bf-empty-page-state>
+  </bf-stage>
+</template>
+
+<script>
+import { mapGetters, mapActions, mapState } from "vuex";
+import BfEmptyPageState from "../../shared/bf-empty-page-state/BfEmptyPageState.vue";
+
+import { pathOr, propOr } from "ramda";
+
+export default {
+  name: "GitHubRepositories",
+
+  components: {
+    BfEmptyPageState,
+  },
+
+  computed: {
+    ...mapGetters(["activeOrganization", "userToken", "config", "hasFeature"]),
+
+    filteredApplications: function () {
+      let filteredArray = this.integrations.filter(
+        (x) => x.customTargets && x.customTargets.length > 0
+      );
+      return filteredArray;
+    },
+
+    hasAdminRights: function () {
+      if (this.activeOrganization) {
+        const isAdmin = propOr(false, "isAdmin", this.activeOrganization);
+        const isOwner = propOr(false, "isOwner", this.activeOrganization);
+        return isAdmin || isOwner;
+      } else {
+        return null;
+      }
+    },
+    orgName: function () {
+      return pathOr("", ["organization", "name"], this.activeOrganization);
+    },
+  },
+
+  watch: {},
+
+  beforeRouteEnter(to, from, next) {
+    next((vm) => {
+      if (vm.hasFeature("sandbox_org_feature")) {
+        vm.$router.push({ name: "create-org" });
+      }
+    });
+  },
+};
+</script>
+
+<style scoped lang="scss">
+
+.empty {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+</style>
---
+++ src/router/releases/Releases.vue
@@ -0,0 +1,67 @@
+<template>
+  <bf-page>
+    <bf-rafter slot="heading" title="Releases" class="primary" :org-id="orgId">
+      <template #description>
+        <div class="description">
+          <p>
+            List of Github repositories, and the github type datasets for the organization
+          </p>
+        </div>
+      </template>
+      <template #tabs>
+        <router-tabs :tabs="tabs" />
+      </template>
+    </bf-rafter>
+    <router-view name="stage"/>
+  </bf-page>
+</template>
+
+<script>
+import { mapState } from "vuex";
+import BfPage from "../../components/layout/BfPage/BfPage.vue";
+import BfStage from "../../components/layout/BfStage/BfStage.vue";
+import BfRafter from "../../components/shared/bf-rafter/BfRafter.vue";
+import RouterTabs from "../../components/shared/routerTabs/routerTabs.vue";
+
+export default {
+  name: "PennsieveReleases",
+  components: {
+    RouterTabs,
+    BfPage,
+    BfStage,
+    BfRafter,
+  },
+  // From Router
+  props: {
+    orgId: {
+      type: String,
+      default: "",
+    },
+    datasetId: {
+      type: String,
+      default: "",
+    },
+  },
+  data() {
+    return {
+      tabs: [
+        {
+          name: "Github Repositories",
+          to: "github-repos",
+        },
+        {
+          name: "Github Datasets",
+          to: "github-datasets",
+        },
+      ],
+    };
+  },
+};
+</script>
+
+<style scoped lang="scss">
+
+.description {
+  max-width: 600px;
+}
+</style>
