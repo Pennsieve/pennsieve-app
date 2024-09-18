6e8c74a4f7eac00627f52c7e7de3a2950195b5a0
vijay kumar, samayamantula <vijay@sparc.science>
2024-09-18 15:36:01 -0400

    update cofigure code repos

--- src/components/Code/ConfigureRepo.vue
+++ src/components/Code/ConfigureRepo.vue
@@ -1,89 +1,169 @@
 <template>
   <bf-stage class="configure-repo-wrapper">
-    <div class="input-item">
-      <el-checkbox
-        v-model="isAutoPublished"
-        label="Automatic publication"
-        size="large"
-      >
-      </el-checkbox>
-      <p>Automatically publish a version on new releases from GitHub</p>
-    </div>
-    <div class="input-item repo-name">
-      <label for="repoName">Repository Name</label>
-      <el-input
-        v-model="repoName"
-        style="width: 320px"
-        placeholder="Repository Name (from GitHub)"
-      />
-    </div>
-    <div class="input-item given-name">
-      <label for="givenName">Name (Required)</label>
-      <el-input
-        v-model="givenName"
-        style="width: 320px"
-        placeholder="user-test"
-      />
-    </div>
-    <div class="input-item subtitle">
-      <label for="subtitle">Subtitle (required to publish)</label>
-      <el-input
-        v-model="subtitle"
-        style="width: 320px"
-        :rows="3"
-        autosize
-        type="textarea"
-        placeholder="any additional details here"
-      />
-    </div>
-    <div class="input-item tags">
-      <label for="tags">Tags</label>
-      <el-input
-              v-model="tags"
-              placeholder="Add tags"
-              style="width: 320px"
-            >
-              <template #prefix>
-                <IconTag
-                  :height="20"
-                  :width="20"
-                  />
-              </template>
-            </el-input>
-    </div>
+    <el-form
+      :model="form"
+      label-width="120px"
+      label-position="top"
+      class="form-auto-save"
+    >
+      <el-form-item class="auto-publish-input">
+        <el-checkbox v-model="form.isAutoPublished" @change="saveForm"
+          >Automatic publication</el-checkbox
+        >
+        <p>Automatically publish a version on new releases from GitHub</p>
+      </el-form-item>
+
+      <el-form-item>
+        <template #label>
+          Repository Name
+          <span class="label-helper"> (GitHub) </span>
+        </template>
+        <el-input v-model="form.repoName" disabled />
+      </el-form-item>
+
+      <el-form-item>
+        <template #label>
+          Name
+          <span class="label-helper"> required </span>
+        </template>
+        <el-input
+          v-model="form.givenName"
+          placeholder="user-test"
+          @input="saveForm"
+        />
+      </el-form-item>
+
+      <el-form-item>
+        <template #label>
+          Subtitle
+          <span class="label-helper"> required to publish </span>
+        </template>
+        <character-count-input
+          v-model="form.description"
+          :rows="3"
+          placeholder="Add a description to help others understand what's in your code repo"
+          @input="saveForm"
+        />
+      </el-form-item>
+
+      <el-form-item id="inputTags">
+        <template #label> Tags </template>
+        <el-input
+          ref="inputTags"
+          v-model="inputTag"
+          placeholder="Add tags"
+          @keyup.enter.native.stop="addTag"
+        >
+          <template #prefix>
+            <IconTag :height="20" :width="20" />
+          </template>
+        </el-input>
+
+        <div class="tag-wrap">
+          <bf-tag
+            v-for="tag in form.tags"
+            :key="tag"
+            :label="tag"
+            class="mr-8 mb-8"
+            @remove="removeTag(tag)"
+          />
+        </div>
+      </el-form-item>
+    </el-form>
   </bf-stage>
 </template>
 
 <script lang="ts" setup>
-import { ref } from "vue";
+import { onMounted, ref } from "vue";
+import { ElMessage } from "element-plus";
+import { useRoute } from "vue-router";
+import debounce from "lodash.debounce";
+import {compose, defaultTo, toLower, trim} from 'ramda'
 
-defineOptions({
-  name: "ConfigureRepo",
+const route = useRoute();
+const form = ref({
+  isAutoPublished: false,
+  repoName: "",
+  givenName: "",
+  description: "",
+  tags: "",
+});
+// let inputTag = ref(''); 
+
+onMounted(() => {
+  if (route.params.repoName) {
+    form.value.repoName = route.params.repoName;
+  }
 });
 
-const isAutoPublished = ref(false);
-const repoName = ref('')
-const givenName = ref('')
-const subtitle = ref('')
-const tags = ref('')
+const saveForm = debounce(function () {
+  {
+    console.log("Saving form data", form.value);
+    ElMessage({
+      message: "Form saved successfully!",
+      type: "success",
+    });
+  }
+}, 1000);
+// Method to check if the tag exists
+const checkIfTagExists = (tag) => {
+  const formTags = propOr([], 'tags', form.value);
+  const tagExistsForm = includes(tag, formTags);
+
+  const datasetTags = pathOr([], ['dataset', 'tags'], dataset.value);
+  const tagExistsDataset = includes(tag, datasetTags);
+
+  return tagExistsForm || tagExistsDataset;
+};
+
+/**
+ * Add tag to the dataset
+ */
+const addTag = function () {
+  const tag = compose(trim, toLower, defaultTo(""))(this.inputTag);
+
+  if (tag) {
+    this.inputTag = "";
+
+    const tagExists = this.checkIfTagExists(tag);
+    if (tagExists === false) {
+      this.form.tags.push(tag);
+      // this.saveForm();
+    }
+  }
+};
+
+
 </script>
 
 <style lang="scss">
 .configure-repo-wrapper {
+  .form-auto-save p {
+    margin-bottom: 0px;
+  }
+
+  .el-form-item__content {
+    flex-direction: column;
+    align-items: start;
+  }
+
   .input-item {
     margin-bottom: 24px;
   }
 
-  span {
-    font-weight: 700;
+  .label-helper {
+    color: $gray_4;
+    font-size: 12px;
+    font-weight: 400;
   }
 
-  .repo-name, .given-name, .subtitle, .tags {
-    label {
-      display: block;
-      margin-bottom: 8px;
-      font-weight: 800;
-    }
+  .auto-publish-input .el-checkbox__label {
+    font-weight: 500;
+  }
+
+  .el-input,
+  .el-textarea {
+    max-width: 475px;
   }
 }
 </style>
