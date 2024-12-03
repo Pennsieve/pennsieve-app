c4c8654e3b59766f51de872600d68b2022fcf077
Annalee Switek <annaleejude@gamil.com>
2024-12-03 12:22:30 -0500

    uncommented pagination

--- src/components/Code/MyReposList.vue
+++ src/components/Code/MyReposList.vue
@@ -1,6 +1,6 @@
 <script setup>
 import { ref, computed, onMounted } from "vue";
-import { useStore } from 'vuex';
+import { useStore } from "vuex";
 import BfEmptyPageState from "../shared/bf-empty-page-state/BfEmptyPageState.vue";
 import RepoListItem from "./RepoListItem.vue";
 import ChangeRepoTrackingDialog from "@/components/Code/ChangeRepoTrackingDialog.vue";
@@ -9,38 +9,43 @@ import { find, propEq } from "ramda";
 
 const store = useStore();
 
+const isLoadingRepos = ref(false);
 
-
-const isLoadingRepos = ref(false)
-
-const myReposCount = computed(() => {return store.getters["codeReposModule/myReposCount"]})
-const myReposPaginationParams = computed(() => {return store.getters["codeReposModule/myReposPaginationParams"]})
-const myReposLoaded = computed(() => {return store.getters["codeReposModule/myReposLoaded"]})
-const myRepos = computed(() => {return store.getters["codeReposModule/myRepos"]})
+const myReposCount = computed(() => {
+  return store.getters["codeReposModule/myReposCount"];
+});
+const myReposPaginationParams = computed(() => {
+  return store.getters["codeReposModule/myReposPaginationParams"];
+});
+const myReposLoaded = computed(() => {
+  return store.getters["codeReposModule/myReposLoaded"];
+});
+const myRepos = computed(() => {
+  return store.getters["codeReposModule/myRepos"];
+});
 
 const showEmptyState = computed(() => {
   return myReposLoaded.value && !myRepos.value.length;
-})
+});
 
 onMounted(async () => {
   try {
     isLoadingRepos.value = true;
-    await store.dispatch("codeReposModule/fetchMyRepos",{
+    await store.dispatch("codeReposModule/fetchMyRepos", {
       page: myReposPaginationParams.value.page,
       size: myReposPaginationParams.value.size,
-    })
-
+    });
   } catch (err) {
     console.error(err);
   } finally {
     isLoadingRepos.value = false;
   }
-})
+});
 
 async function onPaginationPageChange(page) {
   try {
     isLoadingRepos.value = true;
-    await store.dispatch("codeReposModule/fetchMyRepos",{
+    await store.dispatch("codeReposModule/fetchMyRepos", {
       page,
       size: this.myReposPaginationParams.size,
     });
@@ -51,17 +56,15 @@ async function onPaginationPageChange(page) {
   }
 }
 
-const showTrackDialog = ref(false)
-const selectedRepo = ref({})
+const showTrackDialog = ref(false);
+const selectedRepo = ref({});
 function openTrackDialog(repoName) {
-  selectedRepo.value = find(propEq('name', repoName), myRepos.value)
-  showTrackDialog.value = true
+  selectedRepo.value = find(propEq("name", repoName), myRepos.value);
+  showTrackDialog.value = true;
 }
 function onChangeRepoTrackingDialogClose() {
-  showTrackDialog.value = false
-
+  showTrackDialog.value = false;
 }
-
 </script>
 
 <template>
@@ -72,16 +75,16 @@ function onChangeRepoTrackingDialogClose() {
         your account to see your available repos.
       </p>
     </bf-empty-page-state>
-<!--    <div class="pagination-container">-->
-<!--      <el-pagination-->
-<!--        v-if="myReposCount"-->
-<!--        :page-size="myReposPaginationParams.size"-->
-<!--        :current-page="myReposPaginationParams.currentPage"-->
-<!--        layout="prev, pager, next"-->
-<!--        :total="myReposCount"-->
-<!--        @current-change="onPaginationPageChange"-->
-<!--      />-->
-<!--    </div>-->
+    <div class="pagination-container">
+      <el-pagination
+        v-if="myReposCount"
+        :page-size="myReposPaginationParams.size"
+        :current-page="myReposPaginationParams.currentPage"
+        layout="prev, pager, next"
+        :total="myReposCount"
+        @current-change="onPaginationPageChange"
+      />
+    </div>
     <div>
       <RepoListItem
         my-repos-view
@@ -92,30 +95,24 @@ function onChangeRepoTrackingDialogClose() {
         @open-track-dialog="openTrackDialog"
       ></RepoListItem>
     </div>
-<!--    <div class="pagination-container">-->
-<!--      <el-pagination-->
-<!--        v-if="myReposCount && myRepos.length > 5"-->
-<!--        :page-size="myReposPaginationParams.size"-->
-<!--        :current-page="myReposPaginationParams.currentPage"-->
-<!--        layout="prev, pager, next"-->
-<!--        :total="myReposCount"-->
-<!--        @current-change="onPaginationPageChange"-->
-<!--      />-->
-<!--    </div>-->
+    <div class="pagination-container">
+      <el-pagination
+        v-if="myReposCount && myRepos.length > 5"
+        :page-size="myReposPaginationParams.size"
+        :current-page="myReposPaginationParams.currentPage"
+        layout="prev, pager, next"
+        :total="myReposCount"
+        @current-change="onPaginationPageChange"
+      />
+    </div>
 
-        <change-repo-tracking-dialog
-          :dialog-visible="showTrackDialog"
-          :start-tracking-mode="!selectedRepo.tracking"
-          :stop-tracking-mode="selectedRepo.tracking"
-          :repo="selectedRepo"
-          @close="onChangeRepoTrackingDialogClose"
-        />
-<!--        <publish-code-repo-dialog-->
-<!--          :dialog-visible="isPublishCodeRepoDialogVisible"-->
-<!--          :repo="repo"-->
-<!--          @close="onPublishCodeRepoDialogVisibleClose"-->
-<!--        />-->
-
+    <change-repo-tracking-dialog
+      :dialog-visible="showTrackDialog"
+      :start-tracking-mode="!selectedRepo.tracking"
+      :stop-tracking-mode="selectedRepo.tracking"
+      :repo="selectedRepo"
+      @close="onChangeRepoTrackingDialogClose"
+    />
   </bf-stage>
 </template>
 
