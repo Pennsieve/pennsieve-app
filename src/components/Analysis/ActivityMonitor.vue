f66bf984327a85797215945dee57f63be606fd07
Annalee Switek <annaleejude@gamil.com>
2024-12-19 18:21:34 -0500

    displaying active workflows

--- src/components/Analysis/ActivityMonitor.vue
+++ src/components/Analysis/ActivityMonitor.vue
@@ -1,12 +1,21 @@
 <script setup>
-// Importing functions from Vue
-import { ref, computed, onMounted } from "vue";
+import { computed, watch, nextTick, ref, onMounted } from "vue";
 import { useStore } from "vuex";
-import SidePanel from "../datasets/records/GraphBrowser/SidePanel.vue";
-import ModelDetails from "../datasets/records/GraphBrowser/ModelDetails.vue";
-import ModelInfoPanel from "../datasets/records/GraphBrowser/ModelDetails.vue";
-import ModelsList from "../datasets/records/ModelsList/ModelsList.vue";
 
+// Vue Flow Imports
+import { MarkerType, useVueFlow, VueFlow } from "@vue-flow/core";
+import { Background } from "@vue-flow/background";
+import { Controls } from "@vue-flow/controls";
+import { MiniMap } from "@vue-flow/minimap";
+import { useLayout } from "../datasets/records/GraphBrowser/useLayout";
+import { useRoute } from "vue-router";
+import ActivitySidePanel from "./ActivitySidePanel.vue";
+
+const route = useRoute();
+const { layout } = useLayout();
+
+const { onInit, onConnect, addEdges, fitView, findNode, getSelectedNodes } =
+  useVueFlow();
 // Access the Vuex store
 const store = useStore();
 
@@ -26,13 +35,52 @@ const workflowLogs = computed(
 // Local State
 const isLoading = ref(false);
 
-// Methods
-const isActive = (workflow) => {
-  // if the workflow does not contain a completedAt property, it is active
-  return !workflow.completedAt ? "Active" : "Completed";
-};
+const initialNodes = [
+  {
+    id: "1",
+    type: "input",
+    data: { label: "Preprocessor" },
+    position: { x: 130, y: 100 },
+    class: "light",
+  },
+  {
+    id: "2",
+    data: { label: "Processor" },
+    position: { x: 150, y: 250 },
+    class: "light",
+  },
+  {
+    id: "3",
+    type: "output",
+    data: { label: "Postprocessor" },
+    position: { x: 170, y: 400 },
+    class: "light",
+  },
+];
 
-// Lifecycle Hook
+const initialEdges = [
+  {
+    id: "e1-1",
+    source: "1",
+    target: "2",
+    animated: true,
+  },
+  {
+    id: "e1-2",
+    source: "2",
+    target: "3",
+    animated: true,
+  },
+];
+
+const nodes = ref(initialNodes);
+
+const edges = ref(initialEdges);
+
+/*
+Fetch Initial Data
+*/
+
 onMounted(async () => {
   // fetch all workflow instances
   try {
@@ -57,7 +105,6 @@ onMounted(async () => {
   //   isLoading.value = false;
   //   // console.log(workflowInstance.value);
   // }
-
   // fetch logs for one workflow instance
   // try {
   //   isLoading.value = true;
@@ -72,32 +119,136 @@ onMounted(async () => {
   //   console.log("workflowFlow Logs", workflowLogs.value);
   // }
 });
+
+/*
+VueFlow Config and Create Relationships
+ */
+const minimapLocation = computed(() => {
+  if (sidePanelVisible.value) {
+    return "bottom-left";
+  }
+
+  return "bottom-right";
+});
+/*
+Side Panel Logic
+ */
+const sidePanelVisible = ref(true);
+function onTogglePanelVisibility() {
+  sidePanelVisible.value = !sidePanelVisible.value;
+}
 </script>
 
 <template>
-  <!-- Show loading indicator -->
-  <div v-if="isLoading">Loading...</div>
+  <div class="graph-browser">
+    <div class="vue-flow-wrapper">
+      <VueFlow
+        :nodes="nodes"
+        :edges="edges"
+        class="basic-flow"
+        :default-viewport="{ zoom: 1 }"
+        :min-zoom="0.2"
+        :max-zoom="4"
+      >
+        <Background pattern-color="#aaa" :gap="16" />
 
-  <!-- Display API data -->
-  <div v-else>
-    <ul>
-      <li v-for="(workflow, index) in workflowInstances" :key="index">
-        {{
-          `Workflow Uuid: ${workflow.uuid}, Workflow Running: ${isActive(
-            workflow
-          )}`
-        }}
-      </li>
-    </ul>
-    <!-- <ModelInfoPanel />
-    <ModelDetails /> -->
-    <models-list
-      :show-heading="false"
-      :is-link="false"
-      :should-reset.sync="resetModelsList"
-      :scrolling-list="true"
-      @click="clickModel"
+        <!-- <MiniMap :position="minimapLocation" :pannable="true" /> -->
+
+        <Controls position="top-left" />
+      </VueFlow>
+    </div>
+
+    <ActivitySidePanel
+      :class="{ visible: sidePanelVisible }"
+      :panel-visible="sidePanelVisible"
+      @toggle-panel-visibility="onTogglePanelVisibility"
     />
-    <!-- <SidePanel /> -->
   </div>
 </template>
+
+<style lang="sass">
+
+
+/* these are necessary styles for vue flow */
+@import '@vue-flow/core/dist/style.css'
+
+/* this contains the default theme, these are optional styles */
+@import '../..//assets/_vueflow_core.scss'
+@import '../../assets/_vueflow.css'
+
+@import '@vue-flow/minimap/dist/style.css'
+</style>
+
+<style lang="scss" scoped>
+@import "../../assets/_variables.scss";
+
+.modified-stage {
+  margin: 0;
+}
+
+.graph-browser {
+  height: calc(100vh - 114px);
+  overflow: hidden;
+  position: relative;
+}
+
+.vue-flow-wrapper {
+  width: 100%;
+  height: 100%;
+  position: relative;
+}
+
+.action-button {
+  position: absolute;
+  right: 16px;
+  top: 16px;
+
+  &.second {
+    right: 170px;
+  }
+}
+
+.btn-toggle-model-details {
+  align-items: center;
+  background: $gray_1;
+  border-left: 1px solid $gray_2;
+  border-right: 1px solid $gray_2;
+  border-top: 1px solid $gray_2;
+  border-bottom: 1px solid $gray_2;
+  border-radius: 4px 0 0 4px;
+  display: flex;
+  height: 48px;
+  left: -35px;
+  justify-content: center;
+  position: absolute;
+  top: 88px;
+  width: 35px;
+}
+
+.btn-toggle-models-list {
+  align-items: center;
+  background: $gray_1;
+  border: 1px solid $gray_2;
+  border-radius: 4px 0 0 4px;
+  display: flex;
+  height: 48px;
+  left: -35px;
+  justify-content: center;
+  position: absolute;
+  top: 33px;
+  width: 35px;
+
+  &.selected {
+    &:after {
+      background: $gray_1;
+      content: "";
+      height: 100%;
+      pointer-events: none;
+      position: absolute;
+      top: 0;
+      right: -5px;
+      width: 5px;
+    }
+  }
+}
+</style>
---
+++ src/components/Analysis/WorkflowsListItem.vue
@@ -0,0 +1,147 @@
+<template>
+  <div v-if="isActive" class="box green-circle">
+    <div class="icon">●</div>
+    <div class="text">
+      <div class="workflow-name">
+        Workflow UUID: <br />
+        {{ workflow.uuid }}
+      </div>
+      <div class="compute-node-name">
+        Compute Node UUID: {{ workflow.computeNode.uuid }}
+      </div>
+    </div>
+  </div>
+
+  <div v-if="!isActive" class="box red-x">
+    <div class="icon">✖</div>
+    <div class="text">
+      <div class="workflow-name">
+        Workflow UUID: <br />
+        {{ workflow.uuid }}
+      </div>
+      <div class="compute-node-name">
+        Compute Node UUID: {{ workflow.computeNode.uuid }}
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "WorkflowsListItem",
+
+  props: {
+    workflow: {
+      type: Object,
+      default: {},
+    },
+    isActive: {
+      type: Boolean,
+      default: true,
+    },
+  },
+
+  data() {
+    return {};
+  },
+
+  computed: {},
+
+  mounted: function () {
+    console.log("workflow", this.workflow);
+  },
+
+  methods: {},
+};
+</script>
+
+<style lang="scss" scoped>
+@import "../../assets/_variables";
+
+/* Common styles for the box */
+.box {
+  display: flex;
+  align-items: center;
+  padding: 10px;
+  margin: 5px 0;
+  border: 1px solid #ccc;
+  border-radius: 5px;
+  font-family: Arial, sans-serif;
+  font-size: 14px;
+  line-height: 1.4;
+  color: #000;
+  background-color: #fff;
+  width: 425px; /* Adjust the width as needed */
+}
+
+/* Style for the icon (circle or X) */
+.icon {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin-right: 10px;
+  width: 20px;
+  height: 20px;
+  border-radius: 50%;
+  font-size: 16px;
+}
+
+/* Green circle box styles */
+.box.green-circle {
+  border-color: #0f0; /* Green border */
+}
+
+.box.green-circle .icon {
+  background-color: #0f0; /* Green circle background */
+  color: #fff; /* White icon color */
+}
+
+/* Red X box styles */
+.box.red-x {
+  border-color: #f00; /* Red border */
+}
+
+.box.red-x .icon {
+  background-color: #f00; /* Red circle background */
+  color: #fff; /* White icon color */
+}
+
+/* Text styles (Optional, if further customization is needed) */
+.text {
+  display: flex;
+  flex-direction: column;
+}
+
+.text .workflow-name {
+  font-weight: bold;
+}
+
+.text .compute-node-name {
+  font-style: italic;
+  color: #666; /* Gray text */
+}
+
+.concepts-list-item {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+
+  .concept-list-count {
+    color: $gray_6;
+  }
+}
+.model-name {
+  overflow: hidden;
+  span {
+    display: block;
+    overflow: hidden;
+    text-overflow: ellipsis;
+    white-space: nowrap;
+  }
+}
+</style>
+<style>
+.concepts-list-item-tooltip {
+  max-width: 200px;
+}
+</style>
