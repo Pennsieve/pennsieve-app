69bc1675da83961840c6dd1e4f0d5027840823ba
Annalee Switek <annaleejude@gamil.com>
2025-04-15 15:18:24 -0400

    rewrote fetchDatasets method with async await for legibility

--- src/store/index.js
+++ src/store/index.js
@@ -349,6 +349,7 @@ export const mutations = {
       },
 
       SET_DATASET_PUBLISHED_DATA(state, data) {
+        console.log('SET_DATASET_PUBLISHED_DATA', data)
         state.datasetPublishedData = data;
       },
 
