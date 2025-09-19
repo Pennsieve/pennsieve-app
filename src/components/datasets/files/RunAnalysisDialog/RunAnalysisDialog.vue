99ad52a5f8bf90dd21e79b11da5b402af74f9189
Annalee Switek <annaleejude@gamil.com>
2025-09-19 10:39:23 -0400

    fixed async on run analysis request

--- src/components/datasets/files/RunAnalysisDialog/RunAnalysisDialog.vue
+++ src/components/datasets/files/RunAnalysisDialog/RunAnalysisDialog.vue
@@ -596,23 +596,34 @@ export default {
       const token = await useGetToken();
 
       try {
-        this.sendXhr(url, {
+        const response = await this.sendXhr(url, {
           method: "POST",
-          header: {
+          headers: {
+            // Note: should be "headers", not "header"
             Authorization: `Bearer ${token}`,
           },
           body: body,
         });
 
-        EventBus.$emit("toast", {
-          detail: {
-            msg: "Your workflow has been successfully initiated!",
-            type: "success",
-          },
-        });
+        // Check if the response indicates success
+        if (response.ok || (response.status >= 200 && response.status < 300)) {
+          EventBus.$emit("toast", {
+            detail: {
+              msg: "Your workflow has been successfully initiated!",
+              type: "success",
+            },
+          });
+        } else {
+          throw new Error(`HTTP ${response.status}: ${response.statusText}`);
+        }
       } catch (err) {
         console.error(err);
-        this.closeDialog();
+        EventBus.$emit("toast", {
+          detail: {
+            msg: "Failed to initiate workflow. Please try again.",
+            type: "error",
+          },
+        });
       } finally {
         this.closeDialog();
         this.targetDirectory = "";
