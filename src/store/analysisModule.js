91d5d33007f99b19730655aa29cb49fc03ea4e63
Annalee Switek <annaleejude@gamil.com>
2025-09-19 10:12:24 -0400

    in progress

--- src/store/analysisModule.js
+++ src/store/analysisModule.js
@@ -7,6 +7,7 @@ const initialState = () => ({
     applicationsLoaded: false,
     processors: [],
     preprocessors:[],
+    postprocessors: [],
     selectedWorkflow: {},
     selectedProcessor: {},
     selectedFilesForAnalysis: [],
